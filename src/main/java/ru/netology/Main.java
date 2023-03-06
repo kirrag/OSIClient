133ed2a72acbad2784b7610d1a95a703d543cfeb
Garrik Ten <ten@otux.ru>
2023-03-07 00:47:48 +0300

    OSIClient v2.0

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,10 +2,11 @@ package ru.netology;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 		String host = "127.0.0.1";
 		int port = 8089;
 
@@ -13,10 +14,29 @@ public class Main {
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-			out.println("Netology");
+			TimeUnit.SECONDS.sleep(4);
+			System.out.println("- Hi, I'm Client. Let's talk to you. What's your name?");
+			out.println("Client");
 
+			TimeUnit.SECONDS.sleep(4);
 			String resp = in.readLine();
-			System.out.println(resp);
+			System.out.println("- " + resp);
+
+			TimeUnit.SECONDS.sleep(4);
+			System.out.println("- Great, I'm from the fire fox tribe, my version is 110.0.");
+			out.println("req1");
+
+			resp = in.readLine();
+			System.out.println("- " + resp);
+
+			TimeUnit.SECONDS.sleep(4);
+			System.out.println("- Cool, you're too old for me too. Then goodbye. Good refactoring to you, Server.");
+			out.println("req2");
+
+			resp = in.readLine();
+			System.out.println("- " + resp);
+
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
